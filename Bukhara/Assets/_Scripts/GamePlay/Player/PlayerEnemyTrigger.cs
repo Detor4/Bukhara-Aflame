771a7ff5ae8ad3e31d2d38bbe33db53c707ef072
Abdufattoh <abdufattohcoder2004@gmail.com>
2025-04-13 14:14:46 +0500

    add anim

--- Bukhara/Assets/_Scripts/GamePlay/Player/PlayerEnemyTrigger.cs
+++ Bukhara/Assets/_Scripts/GamePlay/Player/PlayerEnemyTrigger.cs
@@ -1,18 +1,53 @@
 ﻿using UnityEngine;
+using Cysharp.Threading.Tasks;
+using UnityEngine.UI;
+using System;
 
 namespace _Scripts.GamePlay.Player
 {
     public class PlayerEnemyTrigger : MonoBehaviour
     {
+        [SerializeField] private Animator animator;
+        [SerializeField] private GameObject gameOverPanel;
+        [SerializeField] private MonoBehaviour playerControllerScript;
+
+        private bool isDead = false;
+
         private void OnTriggerEnter(Collider other)
         {
-            // Agar other obyektida Enemy script bo'lsa
+            if(isDead) return;
+
             Enemy enemyScript = other.GetComponent<Enemy>();
-            if (enemyScript != null)
+
+            if(enemyScript != null)
             {
-                Debug.Log("ENemy");
-                //Destroy(gameObject);
+                
+
+                isDead = true;
+
+                // Animatordagi Die true bo‘lsin
+                if(animator != null)
+                {
+                    animator.SetBool("Die", true);
+                    Debug.Log("Enemy collided!");
+                }
+
+
+                // PlayerController scriptni o‘chirish
+                if(playerControllerScript != null)
+                    playerControllerScript.enabled = false;
+
+                // 5 soniyadan keyin GameOver panel ko‘rsatish
+                ShowGameOverPanelDelayed().Forget();
             }
+        }
+
+        private async UniTaskVoid ShowGameOverPanelDelayed()
+        {
+            await UniTask.Delay(TimeSpan.FromSeconds(5));
+
+            if(gameOverPanel != null)
+                gameOverPanel.SetActive(true);
         }
     }
 }
