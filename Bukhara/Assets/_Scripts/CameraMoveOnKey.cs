7f6df00692746613a0b44a8abdbf13374c92ef75
Abdufattoh <abdufattohcoder2004@gmail.com>
2025-04-15 19:44:30 +0500

    add camera scripts

---
+++ Bukhara/Assets/_Scripts/CameraMoveOnKey.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using Cinemachine;
+using DG.Tweening;
+
+public class CameraMoveOnKey : MonoBehaviour
+{
+    public CinemachineVirtualCamera virtualCamera;
+    public float riseAmount = 5f;      // Necha birlik tepaga ko‘tarilsin
+    public float duration = 5f;        // Qancha vaqt tursa
+    public float moveDuration = 1f;    // Harakat davomiyligi
+
+    private Vector3 originalOffset;
+    private bool isMoving = false;
+
+    void Start()
+    {
+        // Asl Follow Offset ni saqlab qo‘yamiz
+        originalOffset = GetTransposer().m_FollowOffset;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q) && !isMoving)
+        {
+            isMoving = true;
+
+            var transposer = GetTransposer();
+            Vector3 targetOffset = originalOffset + new Vector3(0, riseAmount, 0);
+
+            // Y o‘q bo‘yicha ko‘tarish
+            DOTween.To(() => transposer.m_FollowOffset,
+                       x => transposer.m_FollowOffset = x,
+                       targetOffset,
+                       moveDuration)
+                .SetEase(Ease.OutSine)
+                .OnComplete(() =>
+                            {
+                                // Turgandan keyin pastga tushish
+                                DOVirtual.DelayedCall(duration, () =>
+                                                                {
+                                                                    DOTween.To(() => transposer.m_FollowOffset,
+                                                                               x => transposer.m_FollowOffset = x,
+                                                                               originalOffset,
+                                                                               moveDuration)
+                                                                        .SetEase(Ease.InSine)
+                                                                        .OnComplete(() => isMoving = false);
+                                                                });
+                            });
+        }
+    }
+
+    private CinemachineTransposer GetTransposer()
+    {
+        return virtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+    }
+}
